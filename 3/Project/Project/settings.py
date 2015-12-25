f201b567fca7a6402453f8f80a34da27e53e7e63
w603019289 <603019289@qq.com>
2015-12-25 20:07:22 +0800

    版本3

---
+++ 3/Project/Project/settings.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Django settings for Project project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import socket
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+
+
+MYSQL_DB = 'devicedb'
+MYSQL_USER = 'root'
+MYSQL_PASS = 'a3128103'
+MYSQL_HOST_M = '127.0.0.1'
+MYSQL_HOST_S = '127.0.0.1'
+MYSQL_PORT = '3306'
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'bnr7k9_9m+!rrdt3^p_8i1g8wd%z36$==y2m7co49)3pav#i*#'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = [
+                 '*',
+                 ]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'extend',
+    'pagination',
+)
+
+TEMPLATE_DIRS = (
+'./extend/',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request")
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+
+)
+SESSION_SERIALIZER  = 'django.contrib.sessions.serializers.PickleSerializer'
+ROOT_URLCONF = 'Project.urls'
+
+WSGI_APPLICATION = 'Project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': MYSQL_DB,
+        'USER': MYSQL_USER,
+        'PASSWORD': MYSQL_PASS,
+        'HOST': MYSQL_HOST_S, # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': MYSQL_PORT,
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+          os.path.join(os.path.dirname(__file__), '../static/').replace('\\','/'),
+    )
---
+++ 3/Project/extend/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from extend.models import *
+# Register your models here.
+
+admin.site.register(Belong)
+admin.site.register(User)  
+admin.site.register(Device)
+admin.site.register(Form)
+admin.site.register(AddForm)
